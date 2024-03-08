76814dc5ced80c3309145ef4571aaab70290eb23
pyke9 <tuanpyke99@gmail.com>
2024-03-08 09:54:26 +0700

    first commit

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Chào em, anh có điều muốn nói. </br> Làm người yêu anh nhé!",
+  descriptionHeader:
+    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là đã ĐỒNG Ý.",
+  buttonYes: "Em đồng ý",
+  buttonNo: "Không đồng ý",
+  titleModar: "Cảm ơn em.",
+  descriptionModar:
+    "Cảm ơn em đã cho anh một cơ hội để anh có thể ở bên cạnh, che chở và bảo vệ em. </br> anh biết anh không được đẹp trai, cũng chẳng lắm tài nhưng anh có một trái tim chân thành. em chính là cả thế giới của anh. </br> Tối nay tớ anh đến đón em. Em hãy mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho em.</br> Love you <3",
+};
